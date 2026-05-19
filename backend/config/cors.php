7deba7c4d8f047d81982da258e0f576693121b3d
Kirils Kolesnikovs <166013436+23DP3KKole@users.noreply.github.com>
2026-05-19 19:12:42 +0300

    cors

---
+++ backend/config/cors.php
@@ -0,0 +1,20 @@
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://sheety-mu.vercel.app/'
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
