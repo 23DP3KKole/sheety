78d9337f0564d51da051d05ea7d231403ef43146
Kirils Kolesnikovs <166013436+23DP3KKole@users.noreply.github.com>
2026-05-19 19:06:42 +0300

    axios

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
